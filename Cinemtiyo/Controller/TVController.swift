8f8158932561e13c761aace93c6977a4ee70555f
vinayak <sareenv@uni.coventry.ac.uk>
2019-12-06 15:16:00 +0000

    changes

--- Cinemtiyo/Controller/TVController.swift
+++ Cinemtiyo/Controller/TVController.swift
@@ -8,10 +8,33 @@
 
 import UIKit
 
-class TVController: UITableViewController {
+
+class TVController: UITableViewController{
+
+    let data: [String] = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableViewSettings()
     }
+    
+    fileprivate func tableViewSettings(){
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "TVControllerCell")
+        self.tableView.contentInset = UIEdgeInsets(top: 35,left: 0,bottom: 0,right: 0)
+        tableView.tableFooterView = UIView()
+        tableView.separatorEffect = .none
+    }
+    
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "settingsCell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+    
 }
