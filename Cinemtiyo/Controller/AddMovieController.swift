0ef41611e7db97e7f304eee2e8239e907948e257
vinayak <sareenv@uni.coventry.ac.uk>
2019-12-06 13:42:14 +0000

    changes

--- Cinemtiyo/Controller/AddMovieController.swift
+++ Cinemtiyo/Controller/AddMovieController.swift
@@ -11,17 +11,26 @@ import UIKit
 class AddMovieController: UIViewController{
     
     @IBOutlet weak var movieLabel: UILabel!
-    @IBOutlet weak var movieDescriptionTextField: UITextField!
+    @IBOutlet weak var movieNoteTextView: UITextView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.navigationBar.tintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
-    fileprivate func saveNotesToCoreData(_ movieName: String){
+    @IBAction func addToWatchListButtonPressed() {
+        saveWatchListToCoreData(movieLabel.text ?? "", movieNoteTextView.text ?? "")
+    }
+    
+    fileprivate func saveWatchListToCoreData(_ movieName: String, _ movieNote: String){
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         let watchList = WatchListMovie(context: context)
         watchList.movieName = movieName
+        watchList.movieNote = movieNote
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
     }
 }
