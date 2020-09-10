d5c695a0b0a27b15bfdf7401f38579f0ad81e81c
Vinayak <sareenv026@outlook.com>
2020-09-10 06:08:49 +0530

    trailer fixed and service page organised

---
+++ Cinemtiyo/SwiftUI/SearchView.swift
@@ -0,0 +1,21 @@
+//
+//  SearchView.swift
+//  Cinemtiyo
+//
+//  Created by DataBunker on 10/09/2020.
+//  Copyright © 2020 Vinayak Sareen. All rights reserved.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
